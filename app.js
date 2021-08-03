d69cd2a77042c31622a88df4acc47e8aa2f8a212
Edgar Herrera <edgarherrera@hey.com>
2021-08-03 13:59:36 -0500

    Update dependencies.

--- app.js
+++ app.js
@@ -39,13 +39,6 @@ app.use(passport.initialize())
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
